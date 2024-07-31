e1649889595526984b2f3a8510b39d1525056748
黃振恩 <144233626+nvuhoi@users.noreply.github.com>
2024-07-31 19:10:37 +0800

    Add files via upload

---
+++ lab/lab01/Collatz.java
@@ -0,0 +1,25 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
+ */
+public class Collatz {
+
+    /** if n is odd, return (n * 3 + 1); if n is even, return (n / 2)*/
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return n * 3 + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+}
+
